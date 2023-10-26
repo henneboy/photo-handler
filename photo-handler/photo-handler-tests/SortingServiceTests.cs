b4b3fcec46ddff9bbddc222c8360e7df2f1f6353
henneboy <72788863+henneboy@users.noreply.github.com>
2023-10-26 17:35:05 +0200

    Update SortingServiceTests.cs

--- photo-handler/photo-handler-tests/SortingServiceTests.cs
+++ photo-handler/photo-handler-tests/SortingServiceTests.cs
@@ -10,15 +10,34 @@ public class SortingServiceTests
 	[Fact]
 	public void SortingService_Tests()
 	{
+		// Configuration
+		string fromDir = Path.Combine(_testDataPath, "ToDirEqualData");
+		string toDir = Path.Combine(_testDataPath, "EqualData");
+		HashSet<Criteria> criterias = new()
+		{
+			Criteria.filename
+		};
+
+		// Clean up from previous run
+		File.Delete(fromDir);
+
+		// Assert required input data exists
+		File.Exists(Path.Combine(toDir, "f1.txt")).Should().BeTrue();
+		File.Exists(Path.Combine(toDir, "f2.txt")).Should().BeTrue();
+
+		// Arrange
 		ISortingService ss = new SortingService();
 		State state = new()
 		{
-			Criteria = new HashSet<Criteria>() { Criteria.filename },
-			FromDir = new DirectoryInfo(Path.Combine(_testDataPath, "EqualData")),
-			ToDir = new DirectoryInfo(Path.Combine(_testDataPath, "ToDirEqualData"))
+			Criteria = criterias,
+			FromDir = new DirectoryInfo(toDir),
+			ToDir = new DirectoryInfo(fromDir)
 		};
+
+		// Act
 		ss.PerformSorting(true, state);
-		File.Exists(Path.Combine(_testDataPath, "EqualData", "f1.txt")).Should().BeTrue();
-		File.Exists(Path.Combine(_testDataPath, "EqualData", "f2.txt")).Should().BeTrue();
+
+		// Assert
+
 	}
 }
